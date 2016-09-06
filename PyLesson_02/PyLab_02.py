a236aec83eecb6625eaca082aa34d52bebcd3bcd
spegels <littlepegs21@gmail.com>
2016-09-06 10:23:05 -0700

    PyLab_02

---
+++ PyLesson_02/PyLab_02.py
@@ -0,0 +1,17 @@
+a=4
+b=6
+print("4 multiplied by 6 is",a*b)
+
+Name="Bruce Wayne"
+Address="1234 Arkham Way"
+City="Gotham City"
+Zip="51939"
+print(Name)
+print(Address)
+print(City, Zip)
+
+l=54
+w=6
+h=2
+recArea=l*w*h
+print("The surface area of your rectangle is",recArea)
