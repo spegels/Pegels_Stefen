6f23a01782e19bc369181535e234362599ae7a83
spegels <littlepegs21@gmail.com>
2016-09-12 09:50:25 -0700

    Re-published all

---
+++ QuizPrep.py
@@ -0,0 +1,9 @@
+num=45
+print("IronMan has",num,"kinds of weapons in his suit.")
+pres="Abraham Lincoln"
+quote="\"Whatever you are, be a good one.\""
+print(pres,"once said,",quote)
+h=45
+w=64
+recArea=h*w
+print("The area of your rectangle is...",recArea)
