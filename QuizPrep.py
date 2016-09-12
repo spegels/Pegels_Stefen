6fba30ddd3dfd711b4e311bef567816adb63976b
spegels <littlepegs21@gmail.com>
2016-09-12 10:19:59 -0700

    Added Quiz prep

--- QuizPrep.py
+++ QuizPrep.py
@@ -7,3 +7,13 @@ h=45
 w=64
 recArea=h*w
 print("The area of your rectangle is...",recArea)
+slices=int(input("How many pieces of pizza do you want?"))
+print(slices,"slices, coming right up!")
+name=input("What is your name?")
+print("Is there a Mr. or Mrs."+name+"?")
+
+answer1=input("How many siblings do you have?")
+answer2=input("What are their names?")
+answer3=input("OK, which one is your favorite?")
+print("Awe!"+answer3+"is your favorite.");
+print("R.Kelly said,"I believe I can fly!\"")
