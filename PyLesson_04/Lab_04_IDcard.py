a467c205c15618381e5588daa76622d1cf679994
spegels <littlepegs21@gmail.com>
2016-09-20 10:05:32 -0700

    Added Lab_04

--- PyLesson_04/Lab_04_IDcard.py
+++ PyLesson_04/Lab_04_IDcard.py
@@ -1,32 +1,24 @@
-<<<<<<< HEAD
-=======
-print("******************************")
 
->>>>>>> origin/master
 def idcard(item1,item2):
-    print("*{:^15}{:^15}*".format(item1,item2))
+    print("*{:>15}{:>15}*".format(item1,item2))
 
 item1=input("What organization are you affiliated with?")
 item2=input("What years were you affiliated with said organization?")
-
-<<<<<<< HEAD
 item3=input("Enter your title:")
 item4=input("Enter subject taught/duty:")
-
 item5=input("Enter first name:")
 item6=input("Enter last name:")
 
 print("********************************")
-print("*{:>15}{:>15}*".format(item1,item2))
-print("*{:>15}{:>15}*".format(item5,item6))
-print("*{:>15}{:>15}*".format(item3,item4))
+idcard(item1,item2)
+idcard(item5,item6)
+idcard(item3,item4)
 print("********************************")
 
 
 
 
 
-=======
->>>>>>> origin/master
+
 
     
