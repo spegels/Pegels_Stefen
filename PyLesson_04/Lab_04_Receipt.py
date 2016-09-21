7942eb2a35e5c1d8377f9a8a5044d205780feabf
spegels <littlepegs21@gmail.com>
2016-09-21 09:39:58 -0700

    Adde Lab

--- PyLesson_04/Lab_04_Receipt.py
+++ PyLesson_04/Lab_04_Receipt.py
@@ -12,21 +12,27 @@ price2=float(input("Please enter the price of item 2:"))
 food3=input("Please enter item 3:")
 price3=float(input("Please enter the price of item 3:"))
 
+food4="Subtotal:"
+price4=price1+price2+price3
+
+food5="Tax:"
+price5=float((price1+price2+price3)/12.5)
+
+food6="Total:"
+price6=price1+price2+price3+price5
+
 
 
 print("<<<<<<<<<<<<<<<<Receipt>>>>>>>>>>>>>>>")
 receipt(food1, price1)
 receipt(food2, price2)
 receipt(food3, price3)
-
-print("{:.<15}{:.>15.2f}".format("Subtotal:",price1+price2+price3))
-
-TaxTotal=(price1+price2+price3)/10
-print("{:.<15}{:.>15.2f}".format("Tax:", TaxTotal))
-
-Sale=price1+price2+price3+TaxTotal
-print("{:.<15}{:.>15.2f}".format("Total:",Sale))
+receipt(food4, price4)
+receipt(food5, price5)
+receipt(food6, price6)
 
 print("____________________________________")
 print("*Thank you for your support*")
+
+
 
