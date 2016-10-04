97f4b891961069b6f2267e8a1459efa495c183e1
spegels <littlepegs21@gmail.com>
2016-10-04 10:16:08 -0700

    Circle

--- PyLesson_04/Circle.py
+++ PyLesson_04/Circle.py
@@ -1,10 +1,11 @@
 r=float(input("Enter value of circle's radius:"))
-area=2*3.14*r
+
 def circArea():
     area=float("{:.5f}".format(2*3.14*r))
     return(area)
 def prnt():
     print("The area of a circle with radius",r,"is",area)
+area=2*3.14*r
 
 circArea()
 prnt()
