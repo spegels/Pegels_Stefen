a95bbdac42e7dfff1ffd15ac062084195f42b90a
spegels <littlepegs21@gmail.com>
2016-10-07 10:12:14 -0700

    Added Labs_05

--- PyLesson_05/DiscountReceipt.py
+++ PyLesson_05/DiscountReceipt.py
@@ -1,23 +1,31 @@
 item1=input("Please enter item 1:")
-price1=float(input("Please enter the price of item 1:"))
+price1=int(input("Please enter the price of item 1:"))
 item2=input("Please enter item 2:")
-price2=float(input("Please enter the price of item 2:"))
+price2=int(input("Please enter the price of item 2:"))
 item3=input("Please enter item 3:")
-price3=float(input("Please enter the price of item 3:"))
+price3=int(input("Please enter the price of item 3:"))
 item4=input("Please enter item 4:")
-price4=float(input("Please enter the price of item 4:"))
+price4=int(input("Please enter the price of item 4:"))
 
-subtotal=price1+price2+price3+price4
 
-def discount():
-    if subtotal >= 2000:
-        discount = subtotal*0.15
-    if subtotal < 2000:
-        discount = subtotal*0
-    print(discount)
+
+def discount(su):
+    global discont;
+    if su >= 2000:
+        discont = su * 0.15
+    if su < 2000:
+        discont = 0
+
+subtotal=price1+price2+price3+price4;
+
+discount(subtotal);    
+
+tax=subtotal*0.085
+
+total=subtotal-discont+tax
 
 def format(item,price):
-    print("{:.<10}{:.>10}".format(item,price))
+    print("{:.<10}{:.>10.2f}".format(item,price))
 
 print("<<<<<<<<<<<<<<<Receipt>>>>>>>>>>>>>>>")
 format(item1,price1)
@@ -25,7 +33,9 @@ format(item2,price2)
 format(item3,price3)
 format(item4,price4)
 format("subtotal",subtotal)
-format("discount",discount())
+format("discount",discont)
+format("Tax",tax)
+format("Total",total)
 print("_____________________________________")
 print("Thank you for shopping with us today.")
 
