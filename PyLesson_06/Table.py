683047cfdccce3bbbf6c63b9b5dd2ecab35945a1
spegels <littlepegs21@gmail.com>
2016-10-21 09:28:54 -0700

    Lab_06

--- PyLesson_06/Table.py
+++ PyLesson_06/Table.py
@@ -2,4 +2,4 @@ num = int(input("Enter number: "))
 size = int(input("Enter number of table rows: "))
 
 for i in range(1, size+1):
-    print("{:2.0f}    {:2.0f}".format(i, i*size))
+    print("{:2.0f}    {:2.0f}".format(i, i*num))
