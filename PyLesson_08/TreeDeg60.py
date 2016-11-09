bb77c17311003b391658a7b0410546af793e96de
spegels <littlepegs21@gmail.com>
2016-11-09 10:01:51 -0800

    Lab_08

--- PyLesson_08/TreeDeg60.py
+++ PyLesson_08/TreeDeg60.py
@@ -4,12 +4,14 @@ start = 0
 
 def tree(word, start, stop):
     if start < stop:
-        start += 1
-        print(word[0:start])
+        start += 1     
+        print("{:>{}}".format(word[0:start],stop))
         return tree(word, start, stop)
     else:
         word = ""
         return word
+    
+    
 
 
 print(tree(word, start, stop))
