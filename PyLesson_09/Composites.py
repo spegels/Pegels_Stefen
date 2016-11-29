b292c4bf80b1f033d058ff345b8a3727e73d59a0
spegels <PegelsS0261@sduhsd.lan>
2016-11-29 10:25:26 -0800

    9.1

--- PyLesson_09/Composites.py
+++ PyLesson_09/Composites.py
@@ -1,18 +1,23 @@
 nums = [2,6,8,9,10,12,13,15,17,24,55,66,78,77,79]
-def gfactor():
-    for i in nums:
-        global nums
-        if i % (nums.index(i) + 1) == 0:
+
+def gfactor(num):
+    for i in range(2,num-1,1):
+        if num % i == 0:
             return 1
-        else:
-            return 0
+    return 0
         
 
-def remcomp():
-    for i in nums:
-        if gfactor() == 0:
-            nums.remove(nums[i])
-    return nums
+def remprimes(lst):
+    start = 0
+    for i in range(0,len(lst),1):
+        if gfactor(lst[start]) == 0:
+            lst.pop(start)
+        elif start < len(lst):
+            start +=1
+    print(lst)
 
-remcomp()
-print(nums)
+
+remprimes(nums)
+
+
+
