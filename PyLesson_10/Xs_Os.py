da5c78def0ead61da3f6e9fef49f0b5f4b8fbb56
spegels <PegelsS0261@sduhsd.lan>
2017-01-25 10:22:38 -0800

    Fixed Xs and Os

--- PyLesson_10/Xs_Os.py
+++ PyLesson_10/Xs_Os.py
@@ -5,7 +5,7 @@ xando = []
 for i in range(0,4):
     xando.append([])
     for j in range(0,4):
-        switch = random.randint(1,100)
+        switch = random.randint(0,1)
         if switch == 1:
             xando[i].append("X")
         else:
